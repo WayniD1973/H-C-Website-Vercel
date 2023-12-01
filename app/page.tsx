3cf6043f8c20e433d02eae091f2b81b0ff58b456
WayniD1973 <77841893+WayniD1973@users.noreply.github.com>
2023-12-01 20:55:39 +0000

    Initial commit from IDE

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,120 @@
-import Image from 'next/image'
+import "@/stylesheets/home/style.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+			<section className="carouselCards card-container" id="carouselCards">
+				<div className="productCard card card-hidden productCard1" id="productCard" background="SlideShowBG2.png">
+					<div className="productCardInfo" id="productCardInfo">
+						<h2 className="productCardName title" id="productCardName">Hammy Chat</h2>
+					<p className="productCardDescription" id="productCardDescription">The chat application for all your conversations! Hammy Chat is a very secure and user friendly chat application that can send messages to you or anyone else in seconds. (Closed To Be Redesigned)</p>
+					</div>
+				</div>
+				<div className="productCard card productCard2" id="productCard" background="SlideShowBG1.png">
+					<div className="productCardInfo" id="productCardInfo">
+						<h2 className="productCardName title" id="productCardName">Searchle</h2>
+						<p className="productCardDescription" id="productCardDescription">The self proclaimed most private metasearch engine in the world. No trackers, no nothing. Searchle is completely free and extermely fast! You don't even need an account!</p>
+					</div>
+				</div>
+				<div className="productCard card card-hidden productCard3" id="productCard" background="SlideShowBG3.png">
+					<div className="productCardInfo" id="productCardInfo">
+						<h2 className="productCardName title" id="productCardName">Studio</h2>
+						<p className="productCardDescription" id="productCardDescription">The studio application that does everything, all in one place! H&C Studio fills all of your office and personal needs, whether it's video editing or programming an Operating System, it can do it.</p>
+					</div>
+				</div>
+			</section>
+			<div className="carouselBar" id="carouselBar">
+				<div className="carouselBarItems" id="carouselBarItems">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" className="pauseCarouselButton" id="pauseCarouselButton"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM9 9H11V15H9V9ZM13 9H15V15H13V9Z"></path></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" className="unpauseCarouselButton" id="unpauseCarouselButton"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM10.6219 8.41459L15.5008 11.6672C15.6846 11.7897 15.7343 12.0381 15.6117 12.2219C15.5824 12.2658 15.5447 12.3035 15.5008 12.3328L10.6219 15.5854C10.4381 15.708 10.1897 15.6583 10.0672 15.4745C10.0234 15.4088 10 15.3316 10 15.2526V8.74741C10 8.52649 10.1791 8.34741 10.4 8.34741C10.479 8.34741 10.5562 8.37078 10.6219 8.41459Z"></path></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"className="carouselBarItemArrowLeft" id="carouselBarItemArrowLeft"><path fill="none" d="M0 0h24v24H0z"></path><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg>
+					<div className="carouselBarItem carouselBarItemCircle" id="carouselBarItem1"></div>
+					<div className="carouselBarItem carouselBarItemCircle" id="carouselBarItem2"></div>
+					<div className="carouselBarItem carouselBarItemCircle" id="carouselBarItem3"></div>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" className="carouselBarItemArrowRight" id="carouselBarItemArrowRight"><path fill="none" d="M0 0h24v24H0z"></path><path d="M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z"></path></svg>
+				</div>
+			</div>
+			<section className="recentEvents" id="recentEvents">
+				<hc-grid className="recentEventsGrid" id="recentEventsGrid">
+					<hc-gridItem className="recentEventsGridItem" id="recentEventsGridItem">
+						<div className="recentEventsGridItemImageContainer" id="recentEventsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="recentEventsGridItemTextContainer" id="recentEventsGridItemTextContainer">
+							<p>This is just a grid item in the recent events section of h-and-c.co.uk!</p>
+						</div>
+					</hc-gridItem>
+					<hc-gridItem className="recentEventsGridItem" id="recentEventsGridItem">
+						<div className="recentEventsGridItemImageContainer" id="recentEventsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="recentEventsGridItemTextContainer" id="recentEventsGridItemTextContainer">
+							<p>This is just a grid item in the recent events section of h-and-c.co.uk!</p>
+						</div>
+					</hc-gridItem>
+					<hc-gridItem className="recentEventsGridItem" id="recentEventsGridItem">
+						<div className="recentEventsGridItemImageContainer" id="recentEventsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="recentEventsGridItemTextContainer" id="recentEventsGridItemTextContainer">
+							<p>This is just a grid item in the recent events section of h-and-c.co.uk!</p>
+						</div>
+					</hc-gridItem>
+					<hc-gridItem className="recentEventsGridItem" id="recentEventsGridItem">
+						<div className="recentEventsGridItemImageContainer" id="recentEventsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="recentEventsGridItemTextContainer" id="recentEventsGridItemTextContainer">
+							<p>This is just a grid item in the recent events section of h-and-c.co.uk!</p>
+						</div>
+					</hc-gridItem>
+				</hc-grid>
+			</section>
+			<div className="allProductsHeading-container">
+				<div className="allProductsHeading-line"></div>
+				<h1 className="allProductsHeading-text">All Products</h1> 
+				<div className="allProductsHeading-line"></div>
+			</div>
+			<section className="allProducts" id="allProducts">
+				<hc-grid className="allProductsGrid" id="allProductsGrid">
+					<hc-gridItem className="allProductsGridItem" id="allProductsGridItem">
+						<div className="allProductsGridItemImageContainer" id="allProductsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="allProductsGridItemTextContainer">
+							<h1>Hammy Chat</h1>
+							<p>The chat application for all your conversations! Hammy Chat is a very secure and user friendly chat application that can send messages to you or anyone else in seconds. (Closed To Be Redesigned)</p>
+						</div>
+					</hc-gridItem>
+					<hc-gridItem className="allProductsGridItem" id="allProductsGridItem">
+						<div className="allProductsGridItemImageContainer" id="allProductsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="allProductsGridItemTextContainer">
+							<h1>Searchle</h1>
+							<p>The self proclaimed most private metasearch engine in the world. No trackers, no nothing. Searchle is completely free and extermely fast! You don't even need an account!</p>
+						</div>
+					</hc-gridItem>
+					<hc-gridItem className="allProductsGridItem" id="allProductsGridItem">
+						<div className="allProductsGridItemImageContainer" id="allProductsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="allProductsGridItemTextContainer">
+							<h1>Studio</h1>
+							<p>The studio application that does everything, all in one place! H&C Studio fills all of your office and personal needs, whether it's video editing or programming an Operating System, it can do it.</p>
+						</div>
+					</hc-gridItem>
+					<hc-gridItem className="allProductsGridItem" id="allProductsGridItem">
+						<div className="allProductsGridItemImageContainer" id="allProductsGridItemImageContainer">
+							<img src="SlideShowBG1.png" alt="">
+						</div>
+						<div className="allProductsGridItemTextContainer">
+							<h1>Another Product</h1>
+							<p>Description</p>
+						</div>
+					</hc-gridItem>
+				</hc-grid>
+			</section>
     </main>
   )
 }
